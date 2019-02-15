f3704abf0c85275533451792c4b2c8f7f8fb29b6
Robert Jansen <robjansen15@outlook.com>
2019-02-15 16:09:12 -0500

    Add project files.

---
+++ Schedule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Schedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
